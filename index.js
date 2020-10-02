7ad8234e1b46958cc180cc4423d70acd3f0b4e4a
Felix <jonfelixrico@gmail.com>
2020-10-02 21:35:54 +0800

    Apply corrections to the bootstrapper function

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const { Client } = require('discord.js')
 
+const { DISCORD_TOKEN } = process.env
+
 async function bootstrap() {
   const client = new Client()
-  client.login()
+  try {
+    await client.login(DISCORD_TOKEN)
+    console.info('Logged in succesfully.')
+  } catch (e) {
+    console.error('Something went wrong while logging in.')
+    throw e
+  }
 }
 
-core()
+bootstrap()
