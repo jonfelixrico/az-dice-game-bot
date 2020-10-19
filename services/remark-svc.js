9a026bee114ad4a315f1fd566e13237b3d61ac3b
Jonal Ray Ticug <jonalrayticug@gmail.com>
2020-10-19 09:00:56 +0800

    Added a Random Utility and a Remark Service based on the replies stored in data

---
+++ services/remark-svc.js
@@ -0,0 +1,57 @@
+const RemarkType = require('../enums/remark-type')
+const randomUtil = require('../utils/random')
+
+const gifRepliesContent = require('../data/gif-reply.json')
+const textRepliesContent = require('../data/text-reply.json')
+
+class RemarkSvc {
+  // just in case you have some dependencies
+  constructor(injected) {
+    this.injected = injected
+  }
+
+  /**
+   * Returns a remark to be replied with the user along with what they rolled.
+   *
+   * @param {Object} param0 An object that contains the details of a roll.
+   * @returns {Object} A remark to be embedded in the response to a user's roll.
+   */
+  getRemark({ userId, channelId, rolled, rank, subrank, rollDt }) {
+    // If there is no rank, assign 0
+    if (!rank) {
+      rank = 0
+    }
+
+    // Fetch a random mode between 1 = GIF_URL, 2 = STRING
+    const mode = randomUtil.randomIntRange(0, 1)
+    const remark = { type: null, content: null }
+    
+    let replies = []
+    
+    if (mode == 0) {
+      // show a gif to the user
+      remark.type = RemarkType.GIF_URL,
+      replies = gifRepliesContent[rank]
+    } 
+    
+    if (mode == 1) {
+      // send a text to the user
+      remark.type = RemarkType.STRING,
+      replies = textRepliesContent[rank]
+    }
+
+    if (!replies) {
+      return null
+    }
+
+    const repliesLastItemIndex = replies.length - 1
+    // fetch a random content from the replies based on the type
+    remark.content = replies[randomUtil.randomIntRange(0, repliesLastItemIndex)]
+    return remark
+  }
+}
+
+module.exports = async (injected) => {
+  // do something where. load some stuff, do http calls, etc
+  return new RemarkSvc(injected)
+}
