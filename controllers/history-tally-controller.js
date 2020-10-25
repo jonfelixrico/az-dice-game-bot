d5b62e4b93e0ebdaa40226f2344df6524fe69ad2
Felix <jonfelixrico@gmail.com>
2020-10-25 14:07:01 +0800

    Encase the tally results in a MessageEmbed

--- controllers/history-tally-controller.js
+++ controllers/history-tally-controller.js
@@ -1,5 +1,7 @@
+const { MessageEmbed } = require('discord.js')
 const _ = require('lodash')
 const { sprintf } = require('sprintf-js')
+const { BLANK_SPACE } = require('../enums/string-constants')
 
 class HistoryTallyController {
   constructor({
@@ -22,16 +24,18 @@ class HistoryTallyController {
     }
 
     const { rollEval } = this
+    const sortedRanks = _.chain(rollEval.getRankList())
+      .map(({ rank, subrank, label }) => ({
+        rank: rank || 0,
+        subrank: subrank || 0,
+        label,
+      }))
+      .uniqBy(({ rank, subrank }) => [rank, subrank].join('/'))
+      .sort((a, b) => rollEval.compareEvals(b, a))
+      .value()
 
     const ranks = [
-      ...rollEval
-        .getRankList()
-        .sort((a, b) => rollEval.compareEvals(b, a))
-        .map(({ rank, subrank, label }) => ({
-          rank: rank || 0,
-          subrank: subrank || 0,
-          label,
-        })),
+      ...sortedRanks,
       {
         label: 'No Prize',
         rank: 0,
@@ -43,57 +47,93 @@ class HistoryTallyController {
     return ranks
   }
 
-  async _generateResponse(message) {
-    const channelId = message.channel.id
-
+  async _generateResponseData(channelId) {
     // create map for users per rank
-    const { ranks, total } = await this.hist.usersPerRank(channelId)
-    const indexedWinnersPerRank = _.chain(ranks)
+    const { ranks: ranksWithWinners, total } = await this.hist.usersPerRank(
+      channelId
+    )
+    const indexedWinnersPerRank = _.chain(ranksWithWinners)
       .keyBy(({ rank, subrank }) => [rank, subrank].join('/'))
       .mapValues(({ users, count }) => ({ count, users }))
       .value()
 
-    return (
-      this._ranks
-        // merge list of rank with the users per rank
-        .map((rankData) => {
-          const { rank, subrank } = rankData
-          const key = [rank, subrank].join('/')
-          const winData = indexedWinnersPerRank[key] || { users: [], count: 0 }
-          return {
-            ...rankData,
-            ...winData,
-          }
-        })
-        // format strings
-        .map(({ label, users, count, rank }) => {
-          const percentage = count ? _.round((count / total) * 100, 2) : 0
+    const ranks = this._ranks
+      // merge list of rank with the users per rank
+      .map((rankData) => {
+        const { rank, subrank } = rankData
+        const key = [rank, subrank].join('/')
+        const winData = indexedWinnersPerRank[key] || { users: [], count: 0 }
 
-          const userListStr =
-            rank &&
-            users
-              .map(({ userId, count }) => `> <@${userId}> (${count})`)
-              .join('\n')
+        return {
+          ...rankData,
+          ...winData,
+        }
+      })
 
-          const rankHeaderStr = sprintf(
-            '**%s**: %d (%0.2f%%)',
-            label,
-            count,
-            percentage
+    return {
+      total,
+      ranks,
+    }
+  }
+
+  async _generateEmbedResponse(message) {
+    const channelId = message.channel.id
+    const { total, ranks } = await this._generateResponseData(channelId)
+
+    const fields = ranks.map(({ rank, users, count: rankCount, label }) => {
+      const userListStr =
+        rank &&
+        users
+          .map(({ userId, count: userCount }) =>
+            sprintf('> <@%s> (%d)', userId, userCount)
           )
+          .join('\n')
 
-          return [rankHeaderStr, userListStr].filter((str) => !!str).join('\n')
-        })
-        .join('\n---\n')
-    )
+      let rankSubheaderStr = null
+
+      if (rank) {
+        rankSubheaderStr = sprintf(
+          '%s matching %s.',
+          !rankCount ? 'No' : `**${rankCount}**`,
+          rankCount === 1 ? 'roll' : 'rolls'
+        )
+      } else {
+        rankSubheaderStr = sprintf(
+          '%s %s.',
+          !rankCount ? 'No' : rankCount,
+          rankCount === 1 ? 'roll' : 'rolls'
+        )
+      }
+
+      return {
+        name: label,
+        value: [rankSubheaderStr, userListStr]
+          .filter((str) => !!str)
+          .join('\n'),
+      }
+    })
+
+    return new MessageEmbed({
+      fields,
+      footer: {
+        text: sprintf(
+          '%d %s made so far',
+          total,
+          total === 1 ? 'roll' : 'rolls'
+        ),
+      },
+    })
   }
 
   async handler(message) {
     const channelId = message.channel.id
     await this.executor.queueJob(async () => {
       const reply = await message.channel.send('Loading roll breakdown...')
-      const response = await this._generateResponse(message)
-      await reply.edit(response)
+      const embed = await this._generateEmbedResponse(message)
+      await reply.edit({
+        content: '**Tally of Ranks**',
+        embed,
+      })
     }, channelId)
   }
 }
