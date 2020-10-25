5814d018bb21a88790b39b47bb82b38b5a43820f
Felix <jonfelixrico@gmail.com>
2020-10-25 13:30:11 +0800

    Merge the breakdown and tally command into just the tally command

--- controllers/history-tally-controller.js
+++ controllers/history-tally-controller.js
@@ -1,7 +1,5 @@
-const moment = require('moment-timezone')
-const { table } = require('table')
-const { MessageEmbed } = require('discord.js')
 const _ = require('lodash')
+const { sprintf } = require('sprintf-js')
 
 class HistoryTallyController {
   constructor({
@@ -17,67 +15,85 @@ class HistoryTallyController {
     MessageService.onCommand('!history tally', this.handler.bind(this))
   }
 
-  _generateTableData(tally) {
+  __ranks = null
+  get _ranks() {
+    if (this.__ranks) {
+      return this.__ranks
+    }
+
     const { rollEval } = this
 
-    // create a map for the prize tiers
-    const tierMap = _.chain(rollEval.getRankList())
-      .keyBy(({ rank, subrank }) => [rank || 0, subrank || 0].join('/'))
-      .value()
-
-    // we'll also throw in the prize-less tiers
-    tierMap['0/0'] = { label: 'No Prize' }
-
-    // set the count of the values in tierMap by using the values from `tally`
-    tally.forEach(({ rank, subrank, count }) => {
-      const key = [rank || 0, subrank || 0].join('/')
-      const tier = tierMap[key]
-      tier.count = count
-    })
-
-    /*
-     * sorts ranked rolls from highest to lowest. we cant include no-prize rolls
-     * in this because compareEvals will break
-     */
-    const sortedTally = _.chain(tierMap)
-      .values()
-      .filter(({ rank }) => !!rank)
-      .sort((a, b) => rollEval.compareEvals(b, a))
-      .value()
-
-    // finally, include the no-prize rolls as dead last
-    sortedTally.push(tierMap['0/0'])
-
-    const totalRollsMade = sortedTally.reduce(
-      (acc, { count }) => acc + (count || 0),
-      0
-    )
-
-    const formattedForTable = sortedTally.map(({ label, count }) => [
-      label,
-      // the count of the others may be undefined, so we'll transfrom them to 0
-      count || 0,
-    ])
-
-    return [
-      ['Prize Tier', 'Count'],
-      ...formattedForTable,
-      ['Total rolls made', totalRollsMade],
+    const ranks = [
+      ...rollEval
+        .getRankList()
+        .sort((a, b) => rollEval.compareEvals(b, a))
+        .map(({ rank, subrank, label }) => ({
+          rank: rank || 0,
+          subrank: subrank || 0,
+          label,
+        })),
+      {
+        label: 'No Prize',
+        rank: 0,
+        subrank: 0,
+      },
     ]
+
+    this.__ranks = ranks
+    return ranks
   }
 
-  _generateTallyResponse(tally) {
-    // generate the ascii table for the tally and enclose it within a markdown code block
-    return ['```', table(this._generateTableData(tally)), '```'].join('\n')
+  async _generateResponse(message) {
+    const channelId = message.channel.id
+
+    // create map for users per rank
+    const { ranks, total } = await this.hist.usersPerRank(channelId)
+    const indexedWinnersPerRank = _.chain(ranks)
+      .keyBy(({ rank, subrank }) => [rank, subrank].join('/'))
+      .mapValues(({ users, count }) => ({ count, users }))
+      .value()
+
+    return (
+      this._ranks
+        // merge list of rank with the users per rank
+        .map((rankData) => {
+          const { rank, subrank } = rankData
+          const key = [rank, subrank].join('/')
+          const winData = indexedWinnersPerRank[key] || { users: [], count: 0 }
+          return {
+            ...rankData,
+            ...winData,
+          }
+        })
+        // format strings
+        .map(({ label, users, count, rank }) => {
+          const percentage = count ? _.round((count / total) * 100, 2) : 0
+
+          const userListStr =
+            rank &&
+            users
+              .map(({ userId, count }) => `> <@${userId}> (${count})`)
+              .join('\n')
+
+          const rankHeaderStr = sprintf(
+            '**%s**: %d (%0.2f%%)',
+            label,
+            count,
+            percentage
+          )
+
+          return [rankHeaderStr, userListStr].filter((str) => !!str).join('\n')
+        })
+        .join('\n---\n')
+    )
   }
 
   async handler(message) {
     const channelId = message.channel.id
     await this.executor.queueJob(async () => {
-      const msg = await message.channel.send('Loading tally...')
-
-      const tally = await this.hist.countPerRank(channelId)
-      await msg.edit(this._generateTallyResponse(tally))
+      const reply = await message.channel.send('Loading roll breakdown...')
+      const response = await this._generateResponse(message)
+      await reply.edit(response)
     }, channelId)
   }
 }
